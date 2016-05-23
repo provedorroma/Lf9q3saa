661a5b5e1f4d018355041a210074ab47fa545c00
Leonardo G Mello <leo.galetti@gmail.com>
2016-05-23 17:27:21 -0300

    Commit Inicial

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+function page(){
+	if($("#config_page").is(':visible')){
+		$("#config_page").hide();
+	}else{
+		$("#config_page").show();
+	}
+}
+
+function galeria(){
+	if($("#config_galeria").is(':visible')){
+		$("#config_galeria").hide();
+	}else{
+		$("#config_galeria").show();
+	}
+}
+
+function blog(){
+	if($("#config_blog").is(':visible')){
+		$("#config_blog").hide();
+	}else{
+		$("#config_blog").show();
+	}
+}
+
+function google(){
+	if($("#config_google").is(':visible')){
+		$("#config_google").hide();
+	}else{
+		$("#config_google").show();
+	}
+}
+
+function form(){
+	if($("#config_form").is(':visible')){
+		$("#config_form").hide();
+	}else{
+		$("#config_form").show();
+	}
+}
+
+function config(){
+	if($("#config_all").is(':visible')){
+		$("#config_all").hide();
+	}else{
+		$("#config_all").show();
+	}
+}
